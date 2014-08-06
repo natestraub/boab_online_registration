9ef9ccb674f208b8f7284350cc873fa7feb70a0c
Nathan Straub <nstraub@boabtravel.com>
2014-08-05 22:06:02 -0500

    Implemented sign-up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   root "trips#index"
 
   resources :trips do
     resources :rooms
   end
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ spec/features/signing_up_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'Signing up' do
+	scenario 'Successful sign up' do
+		visit '/'
+
+		click_link 'Sign Up'
+		fill_in "Email", with: "user@example.com"
+		fill_in "Password", with: "password"
+		fill_in "Confirm Password", with: "password"
+		click_button "Sign Up"
+
+		expect(page).to have_content("You have signed up successfully.")
+	end
+end
